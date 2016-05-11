99da438aaff6f4b8b1cfd4306c36a0c380c4499d
fredTimi <tapin.frederic@gmail.com>
2016-05-11 15:34:28 +0200

    RAS

---
+++ core/src/com/mygdx/game/hud/EmptyUI.java
@@ -0,0 +1,13 @@
+package com.mygdx.game.hud;
+
+/**
+ * Created by frédéric on 04/05/2016.
+ */
+public class EmptyUI extends HUD {
+
+    @Override
+    public void act(float deltaTime) {
+
+    }
+
+}
